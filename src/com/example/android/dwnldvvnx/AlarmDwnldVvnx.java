5b88ec443ba5e12c2c65a2d4a670cef64a9f3b15
dnagis <vincent.achard@gmail.com>
2019-09-26 11:44:56 +0200

    request POST

--- src/com/example/android/dwnldvvnx/AlarmDwnldVvnx.java
+++ src/com/example/android/dwnldvvnx/AlarmDwnldVvnx.java
@@ -16,6 +16,21 @@ import android.content.IntentFilter;
 import android.os.Environment;
 import java.io.File;
 
+import android.os.AsyncTask;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.List;
+import org.json.JSONObject;
+
 
 
 
@@ -34,14 +49,17 @@ public class AlarmDwnldVvnx extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 		
-		Log.d(TAG, "OnStartCommand dans AlarmDwnldVvnx");		
+		Log.d(TAG, "OnStartCommand dans AlarmDwnldVvnx");	
+		
+		//POST Request, déporté dans AsyncTask sinon erreuur runtime android.os.NetworkOnMainThreadException
+		new PostRequestTask().execute();		
+		
+		
+			
 		
 		//Téléchargement via DownloadManager
-		DownloadManager downloadmanager = (DownloadManager) getSystemService(this.DOWNLOAD_SERVICE);
-		
-		Uri uri = Uri.parse("http://5.135.183.126:8778/test.img");
-		
-		
+		DownloadManager downloadmanager = (DownloadManager) getSystemService(this.DOWNLOAD_SERVICE);		
+		Uri uri = Uri.parse("http://5.135.183.126:8778/test.img");		
 		/**
 		 * Si tu veux accès au dir: downloadManager n'overwrite pas, tu te retrouves avec des fichier-N.ext
 		 * ce bout de code vide le dir. Il est fait pour aller avec:
@@ -51,25 +69,19 @@ public class AlarmDwnldVvnx extends Service {
 		{
 		for (File child : dir_dest.listFiles())
             child.delete();
-		}*/
-		
-		
+		}*/		
 		//Détails request sur https://developer.android.com/reference/android/app/DownloadManager.Request
 		DownloadManager.Request request = new DownloadManager.Request(uri);
 		request.setTitle("DwnldVvnx");
 		request.setDescription("Downloading");		
 		//request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //bruit + notif barre
 		request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN); //nécessite permission DOWNLOAD_WITHOUT_NOTIFICATION
-		request.setVisibleInDownloadsUi(false);	
-		
+		request.setVisibleInDownloadsUi(false);			
 		//Si je mets aucune destination: fichier doit être en cache qq part et je le vois jamais! (pratique pour tests) 
-		
 		//à décommenter si tu veux avoir accès au fichier (i.e. animation satellite)
 		//request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, "test.img"); //  --> /storage/emulated/0/Android/data/com.example.android.dwnldvvnx/files/Download/
-		
 		//Nécessite	 permission.WRITE_EXTERNAL_STORAGE (manifest + param!!!), je laisse pour info, chiant à utiliser...
 		//request.setDestinationUri(Uri.parse("file:///sdcard/vvnx_files/airmada.pdf")); //il lui faut
-		
 		downloadmanager.enqueue(request);		
 		//Register du receiver DOWNLOAD_COMPLETE -> dans le manifest only, si je le mets ici aussi, tu te frappes deux onReceive() à chaque fois!
 		launchTimestamp = System.currentTimeMillis();
@@ -88,5 +100,46 @@ public class AlarmDwnldVvnx extends Service {
       // We don't provide binding, so return null
       return null;
 	}
+	
+	
+	
+	private class PostRequestTask extends AsyncTask<Void,Void,Void> {
+		//https://alvinalexander.com/android/asynctask-examples-parameters-callbacks-executing-canceling
+    
+	    @Override
+	    protected Void doInBackground(Void... params) {
+			String error_code = "HTTP_REPLY_NON_INITIALISEE";
+			
+			try {	
+			URL url = new URL("http://192.168.1.118:8000/essai_post");
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setDoOutput(true);
+			urlConnection.setRequestMethod("POST");
+			urlConnection.setRequestProperty("Content-Type","application/json");
+			urlConnection.setRequestProperty("charset", "utf-8");
+			DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
+			//out.write(jsonEnvoi.toString().getBytes("iso-8859-15"));
+			String mon_json = "{\"username\":\"vincent\"}";
+			out.writeBytes(mon_json);
+			out.flush();
+			out.close();
+			error_code = urlConnection.getResponseMessage();
+			Log.d(TAG, "Vincent reponse getResponseMessage= " + error_code);	
+	
+	        } catch (MalformedURLException me) {
+	            Log.d(TAG, "MalformedURLException: " + me);
+	
+	        } catch (IOException ioe) {
+	            Log.d(TAG, "IOException: " + ioe);
+	        }
+			return null;
+	    }
+
+	}
+	
+	
+	
 }
+
+
 
